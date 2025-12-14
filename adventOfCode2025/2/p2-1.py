7a49a28148b8be337ee749e1cd24f62a3c2408e6
arcueun <arcueun@gmail.com>
2025-12-14 11:36:56 +1100

    init day2

---
+++ adventOfCode2025/2/p2-1.py
@@ -0,0 +1,10 @@
+# for each number (in the range) simple parsing 
+with open("2.txt", "r") as f:
+    
+
+
+
+# translate it to a string 
+# take the first half of the digits and compare it to the second half 
+# if identical, list the number and increment a counter 
+# duh u win 
